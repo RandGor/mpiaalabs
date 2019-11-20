620720590e0111ac24a3f232c077cb69107fb848
RandGor <amlqn0bkjqtv@mail.ru>
2019-11-20 23:17:00 +0700

    Add files via upload

---
+++ mpiaa4lab/main.cpp
@@ -0,0 +1,67 @@
+#define CATCH_CONFIG_RUNNER
+
+#include "catch.hpp"
+#include "path.h"
+
+typedef std::chrono::high_resolution_clock hrc;
+
+namespace {
+	class Timer {
+	public:
+		Timer() {}
+
+		void start() {
+			begin = hrc::now();
+		}
+
+		double getTime() {
+			return std::chrono::duration<double>(hrc::now() - begin).count();
+		}
+
+	private:
+		hrc::time_point begin;
+	};
+}
+
+Graph gen_random_graph(int size) {
+	Graph g;
+	for (int i = 0; i < 10 * size; i++) {
+		g.add_arc(rand() % size, rand() % size);
+	}
+	return g;
+}
+
+void measure(int size) {
+	const auto g = gen_random_graph(size);
+
+	Timer t;
+
+	t.start();
+	double cc_time = t.getTime();
+
+	t.start();
+	const int repeats = 10;
+	for (int i = 0; i < repeats; i++) {
+		path_exists(g, rand() % size, rand() % size);
+		/*for (int j = 0; j < size - 1; j++)
+		{
+			for (int k = 0; k < j; k++)
+			{
+				path_exists(g, j, k); //только 2 рандомные вершины
+			}
+		}*/
+	}
+	double sp_time = t.getTime() / repeats;
+
+	printf("N: %8d,time: %10.5f sec\n",
+		size,sp_time);
+}
+
+int main() {
+	for (int i = 1; i < 6; i++) {
+		measure(pow(10, i));
+	}
+	return 0;
+
+
+}
---
+++ mpiaa4lab/path.cpp
@@ -0,0 +1,37 @@
+#include "path.h"
+
+bool path_exists(const Graph &graph, int start_vertex, int end_vertex) {
+
+	std::vector<int> queue, list, current_list;
+	int current_vertex;
+	bool is = false;
+
+	if (graph.has_vertex(start_vertex) && graph.has_vertex(end_vertex))
+	{
+		current_vertex = start_vertex;
+		queue.push_back(current_vertex);
+		while (!queue.empty())
+		{
+			current_vertex = queue.back();
+			queue.pop_back();
+			if (current_vertex == end_vertex)
+				return true;
+			current_list = graph.get_adjacent_vertices(current_vertex);
+			for (int i = 0; i < current_list.size(); i++)
+			{
+				is = false;
+				for (int j = 0; j < list.size(); j++)
+					if (list[j] == current_list[i])
+					{
+						is = true;	//optomize
+						break;
+					}
+				
+				if (!is)
+					queue.push_back(current_list[i]);
+			}
+			list.push_back(current_vertex);
+		}
+	}
+	return false;
+}
