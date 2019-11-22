68f5d9f8a1cebb406dc180bb150b755a189a04b4
RandGor <amlqn0bkjqtv@mail.ru>
2019-11-23 00:00:07 +0700

    Add files via upload

--- mpiaa2dz/Source.cpp
+++ mpiaa2dz/Source.cpp
@@ -1,15 +1,82 @@
+#define CATCH_CONFIG_RUNNER
 #include "library.h"
+#include "catch.hpp"
+
+typedef std::chrono::high_resolution_clock hrc;
+
+namespace {
+	class Timer {
+	public:
+		Timer() {}
+
+		void start() {
+			begin = hrc::now();
+		}
+
+		double getTime() {
+			return std::chrono::duration<double>(hrc::now() - begin).count();
+		}
+
+	private:
+		hrc::time_point begin;
+	};
+}
+
+vector<int> findKSS(Graph g) {
+	vector<int> ans;
+
+	// Рёбра
+	int a, b;
+
+
+	while (!in.eof()) {
+		in >> a >> b;
+		g.add_arc(a, b);
+		gr.add_arc(b, a);
+	}
+
+	for (const auto& p : g.get_graph()) {
+		used[p.first] = false;
+	}
+	for (const auto& p : gr.get_graph()) {
+		used[p.first] = false;
+	}
+
+	for (const auto& p : g.get_graph()) {
+		if (!used[p.first])
+			dfs1(p.first);
+	}
+
+
+	for (const auto& p : g.get_graph()) {
+		used[p.first] = false;
+	}
+	for (const auto& p : gr.get_graph()) {
+		used[p.first] = false;
+	}
+
+
+	std::reverse(order.begin(), order.end());
+	for (int p : order) {
+		int v = p;
+		if (!used[v]) {
+			dfs2(v);
+			if (component.size() > 1) {
+				ans = component;
+			}
+			component.clear();
+		}
+	}
+
+	return ans;
+}
+
 int main()
 {
-	setlocale(0, "");
-	int count = 0;
 	ifstream in("input.txt");
 	// если файл не открылся
 	if (!in)
 	{
-		printf_s("Не удалось открыть файл input.txt");
-		in.close();
-		_getch();
 		return 0;
 	}
 
@@ -19,15 +86,7 @@ int main()
 
 	while (!in.eof()) {
 		in >> a >> b;
-		// Проверка присутствуют ли среди вершин отрицательные, не или равные 0.
-		if (a <= 0 || b <= 0) {
-			printf_s("Невозможно построить с заданными вершинами компоненты сильной связанности.\nНомера вершин должны быть положительными и не превышать n");
-			in.close();
-			_getch();
-			return 0;
-		}
-		else 
-			g.add_arc(a, b);
+		g.add_arc(a, b);
 		gr.add_arc(b, a);
 	}
 
@@ -37,16 +96,11 @@ int main()
 	for (const auto &p : gr.get_graph()) {
 		used[p.first] = false;
 	}
-	//int n = g.get_size();
 
 	for (const auto &p : g.get_graph()) {
 		if (!used[p.first])
 			dfs1(p.first);
 	}
-
-	//for (int i = 0; i < n; i++)
-	//	if (!used[i])
-	//		dfs1(i);
 
 
 	for (const auto &p : g.get_graph()) {
@@ -59,11 +113,6 @@ int main()
 
 	std::reverse(order.begin(), order.end());
 	for (int p : order) {
-	//	used[p.first] = false;
-	//}
-
-	//for (int i = 0; i < n; i++)	{
-		//int v = order[n - 1 - i];
 		int v = p;
 		if (!used[v]) {
 			dfs2(v);
@@ -71,16 +120,12 @@ int main()
 				for (size_t j = 0; j < component.size(); j++)
 					fout << component[j] << " ";
 				fout << endl;
-				count++;
 			}
 			component.clear();
 		}
 	}
 	in.close();
 	fout.close();
-	if (count == 0) {
-		printf_s("Не удалось построить ни одной КСС");
-		_getch();
-		return 0;
-	}
+
+	return 1;
 }
