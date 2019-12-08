906d7851b67d781e5f2d1ec34545fa706c314958
RandGor <amlqn0bkjqtv@mail.ru>
2019-12-08 15:05:27 +0700

    Add files via upload

--- mpiaa3dz/electricity.h
+++ mpiaa3dz/electricity.h
@@ -2,11 +2,31 @@
 
 #include <vector>
 #include <algorithm>
-#include "graph.h"
+#include <iostream>
+#include "timer.h"
+
 using namespace std;
 
-/// euristic
-vector<int> gates_exp(int v, int e, int* stations, int** wires);
+class Wire {
+public:
+	int a;
+	int b;
+	Wire(int A, int B);
+};
 
+bool isContains(vector<int> set, int node);
+
+bool isContains(vector<Wire> set, Wire node);
+
+bool isCovers(vector<Wire> wires, vector<int> generators, vector<int> set);
+
+int vertexAdjCount(vector<Wire> wires, int node);
+
+vector<vector<int>> appendix(vector<vector<int>> previous, vector<int> vertexes);
+
+vector<int> getVertexes(vector<Wire> wires);
+
+/// euristic
+vector<int> gates_greedy(vector<Wire> wires);
 /// brute
-vector<int> gates_brute(int v, int e, int* stations, int** wires);
+vector<int> gates_brute(vector<Wire> wires);
